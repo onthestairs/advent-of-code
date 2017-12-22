47b8f72e26fc5343656026ba63d1e256fee71eb2
Austin Platt <austinplatt@gmail.com>
2017-12-22 19:38:54 +0000

    solve 14 part 2

--- src/14.purs
+++ src/14.purs
@@ -2,20 +2,22 @@ module Day14 where
 
 import Prelude
 
-import Data.Array (concat, concatMap, filter, range, mapWithIndex)
-import Data.Foldable (length)
+import Control.Monad.Rec.Class (Step(..), tailRec)
+import Data.Array (concat, concatMap, cons, elem, filter, foldl, mapWithIndex, range, uncons)
+import Data.Array as Array
+import Data.Foldable (class Foldable, length)
 import Data.Graph (unfoldGraph)
 import Data.Graph as Graph
 import Data.Int (binary, fromStringAs, hexadecimal, toStringAs)
 import Data.List (List(..), catMaybes, (:))
 import Data.List as List
+import Data.Map as Map
 import Data.Maybe (Maybe(..), fromMaybe)
-import Data.StrMap as Map
+import Data.Set as Set
 import Data.String (Pattern(..), singleton, split, toCharArray)
-import Data.Tuple (Tuple(..))
+import Data.Tuple (Tuple(..), fst)
 import Data.Tuple.Nested ((/\))
-import Day10 (knotHash)
-import Day12 (connectedRegions)
+import Day10 (knotHash, padLeft)
 import Day5 (readInt)
 import Debug.Trace (spy)
 import Partial.Unsafe (unsafePartial)
@@ -27,8 +29,9 @@ derive instance eqBit :: Eq Bit
 makeChars :: Int -> Array Char
 makeChars n = toCharArray $ key <> "-" <> show n
 
+
 charToBits :: Char -> Array Bit
-charToBits c = map (\c -> if c == '1' then One else Zero) $ toCharArray $ toStringAs binary $ fromMaybe 0 $ fromStringAs hexadecimal (singleton c)
+charToBits c = map (\c -> if c == '1' then One else Zero) $ toCharArray $ padLeft '0' 4 $ toStringAs binary $ fromMaybe 0 $ fromStringAs hexadecimal (singleton c)
 
 hashToBits :: String -> Array Bit
 hashToBits s = concatMap charToBits (toCharArray s)
@@ -44,43 +47,43 @@ solve = length $ filter ((==) One) (concat hashBits)
 
 
 --
--- type Coord = Tuple Int Int
 -- s :: Int -> Int -> String
 -- s row col = show row <> "," <> show col
--- type Bits = Map.StrMap Bit
+type Coord = Tuple Int Int
+type OnBits = Set.Set (Coord)
 --
--- bits :: Bits
--- bits = Map.fromFoldable $ concat $ mapWithIndex (\row bits -> mapWithIndex (\col bit -> Tuple (s row col) bit) bits) hashBits
---   where n = 128
---         hashes = map (knotHash <<< makeChars) (range 0 (n-1))
---         hashBits = map hashToBits hashes
---
--- -- findAdjacents :: Coord -> Array Coord
--- -- findAdjacents (row /\ col) = [
--- --   (row + 1) /\ col,
--- --   (row - 1) /\ col,
--- --   row /\ (col + 1),
--- --   row /\ (col - 1)
--- -- ]
--- findAdjacents :: Partial => String -> Array String
--- findAdjacents t = map (\(row /\ col) -> s row col) [
---   (row + 1) /\ col,
---   (row - 1) /\ col,
---   row /\ (col + 1),
---   row /\ (col - 1)
--- ]
---   where [rowS, colS] = split (Pattern ",") t
---         row = (readInt <<< List.fromFoldable <<< toCharArray) rowS
---         col = (readInt <<< List.fromFoldable <<< toCharArray) colS
---
--- makeGraph :: Bits -> Graph.Graph String String
--- makeGraph bits = unfoldGraph oneCoords id adjacentOneCoords
---   where oneCoords = Map.keys $ Map.filter ((==) One) bits
---         adjacentOneCoords :: String -> Array String
---         adjacentOneCoords c = filter isOne (unsafePartial $ findAdjacents c)
---         isOne :: String -> Boolean
---         isOne t = (Map.lookup t bits) == (Just One)
---
--- solve2 = connectedRegions g
---   where g = makeGraph bits
---         -- blah = spy ((length $ Graph.vertices g) :: Int)
+makeOnBits :: OnBits
+makeOnBits = Set.fromFoldable $ map fst $ filter (\(Tuple _ bit) -> bit == One) $ concat $ mapWithIndex (\row bits -> mapWithIndex (\col bit -> Tuple (row /\ col) bit) bits) hashBits
+  where n = 128
+        hashes = map (knotHash <<< makeChars) (range 0 (n-1))
+        hashBits = map hashToBits hashes
+
+adjacents :: Coord -> Set.Set Coord
+adjacents (row /\ col) = Set.fromFoldable [
+  (row + 1) /\ col,
+  (row - 1) /\ col,
+  row /\ (col + 1),
+  row /\ (col - 1)
+]
+
+popSet :: forall a. Ord a => Set.Set a -> Maybe {value :: a, rest :: Set.Set a}
+popSet s = case Set.findMin s of
+  Nothing -> Nothing
+  Just x -> Just {value: x, rest: Set.delete x s}
+
+connectedBits :: OnBits -> Coord -> Array Coord
+connectedBits onBits c = Array.fromFoldable $ tailRec go {connected: Set.empty, queue: Set.singleton c}
+  where go state = case popSet state.queue of
+                    Nothing -> Done state.connected
+                    Just {value: x, rest: xs} ->
+                      if Set.member x onBits
+                      then Loop {connected: Set.insert x state.connected, queue: Set.union state.queue (Set.difference (adjacents x) state.connected)}
+                      else Loop {connected: state.connected, queue: xs}
+
+connectedRegions :: forall k f. Ord k => Foldable f => (f k) -> (k -> Array k) -> Int
+connectedRegions ns f = length $ foldl (\ms n -> go n ms) [] ns
+  where go n ms = if (elem n $ concat ms) then ms else (cons (f n) ms)
+
+solve2 = connectedRegions onBits (connectedBits onBits)
+  where onBits = makeOnBits
+        -- blah = spy ((length $ Graph.vertices g) :: Int)
