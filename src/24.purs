f90c17997b699559846289660bc5313966136114
Austin Platt <austinplatt@gmail.com>
2017-12-24 12:42:40 +0000

    finish day 24

--- src/24.purs
+++ src/24.purs
@@ -5,11 +5,13 @@ import Prelude
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (logShow)
 import Control.Monad.Eff.Exception (EXCEPTION)
-import Data.Array (concatMap, cons, drop, filter, fromFoldable, length, span)
+import Control.Monad.Rec.Class (tailRec)
 import Data.Either (Either)
 import Data.Foldable (maximum, sum)
+import Data.List (List(..), concatMap, drop, filter, fromFoldable, length, singleton, span, (:))
 import Data.Maybe (Maybe)
 import Data.String (trim)
+import Data.Tuple (Tuple(..))
 import Day2 (parseInt)
 import Node.Encoding (Encoding(..))
 import Node.FS (FS)
@@ -27,7 +29,7 @@ rotate (Component i j) = Component j i
 fst (Component i j) = i
 snd (Component i j) = j
 strength (Component i j) = i + j
-type Input = Array Component
+type Input = List Component
 
 
 parseComponent :: Parser Component
@@ -46,25 +48,27 @@ getInput filename =
 
 -----
 
-takeElem :: forall a. Eq a => Array a -> a -> {c :: a, rest :: Array a}
+takeElem :: forall a. Eq a => List a -> a -> {c :: a, rest :: List a}
 takeElem cs c = case span ((/=) c) cs of
   {init, rest} -> {c: c, rest: init <> (drop 1 rest)}
 
-validComponents :: Int -> Array Component -> Array {c :: Component, rest :: Array Component}
+validComponents :: Int -> List Component -> List {c :: Component, rest :: List Component}
 validComponents n cs = map (takeElem rotated) $ filter (\c -> fst c == n) rotated
   where rotated = map (\c -> if fst c == n then c else rotate c) cs
 
--- makeBridges :: Int -> Array Component -> Array (Array Component)
--- makeBridges n cs = concatMap f possibleComponents
---   where possibleComponents = validComponents n cs
---         f {c, rest} = let ds = makeBridges (snd c) rest in if length ds == 0 then [[c]] else map (cons c) ds
-
-makeBridges :: Int -> Array Component -> Array (Array Component)
+makeBridges :: Int -> List Component -> List (List Component)
 makeBridges n cs = concatMap f possibleComponents
   where possibleComponents = validComponents n cs
-        f {c, rest} = let ds = makeBridges (snd c) rest in if length ds == 0 then [[c]] else map (cons c) ds
+        f {c, rest} = let ds = makeBridges (snd c) rest in if length ds == 0 then singleton (singleton c) else map (\d -> c:d) ds
 
 solve :: Input -> Maybe Int
 solve input = maximum $ map (sum <<< map strength) (makeBridges 0 input)
 
-solve' = (map <<< map) solve (getInput "./src/24.test.txt") >>= logShow
+solve' = (map <<< map) solve (getInput "./src/24.txt") >>= logShow
+
+---------
+
+solve2 :: Input -> Maybe (Tuple Int Int)
+solve2 input = maximum $ map (\bridge -> Tuple (length bridge) (sum $ map strength bridge)) (makeBridges 0 input)
+
+solve2' = (map <<< map) solve2 (getInput "./src/24.txt") >>= logShow
